88b2e769cc9b48ac42681cf84812fe60efa7e48a
Dhivya <cdhivya2000@gmail.com>
2020-11-23 10:55:50 +0530

    third

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var userRouter = require('./routes/users');
 const usersDetails = require('./models/users');
 const deptDetails = require('./models/dept');
 const comtypeDetails = require('./models/comtype');
+const comDetails = require('./models/complaints');
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/SGS';
 const connect = mongoose.connect(url);
@@ -16,7 +18,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(express.static(__dirname+'/public'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,12 +26,7 @@ app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
-app.get('/renderHTML', (req, res) => {
-  res.sendFile('home.html', {
-      root: path.join(__dirname, './')
-  })
-})
+app.use('/user', userRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
+var user = require('../models/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  var pwd1;
+  var uname = req.body.uname;
+  var pwd = req.body.pwd;
+  //console.log(uname);
+  //console.log(pwd);
+  if(uname=='admin')
+  {
+      user.find({'u_name': uname},function(err, teams) {
+      if (err) {
+        console.log(err);
+      } else {
+        //console.log(teams);
+        pwd1 = teams[0].u_pass;
+      }
+      if(pwd == pwd1)
+      res.sendFile('admin.html', {
+         root: path.join(__dirname, '../')
+      });
+      else
+      res.sendFile('error.html', {
+        root: path.join(__dirname, '../')
+     });
+
+      });
+      //console.log(pwd1);
+      
+      
+  }
+  else
+  res.sendFile('student.html', {
+    root: path.join(__dirname, '../')
+  //console.log(fname);
+  });
+  
 });
 
 module.exports = router;
